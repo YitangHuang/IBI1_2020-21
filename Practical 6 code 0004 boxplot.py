421a256b0d290ff2aa2cfce8a876f8398444fe3c
YitangHuang <yitang.20@intl.zju.edu.cn>
2021-04-12 16:06:51 +0800

    add a title to the pie chart and remake the boxplot
    now the boxplot seems much better

--- Practical 6 code 0004 boxplot.py
+++ Practical 6 code 0004 boxplot.py
@@ -1,15 +1,14 @@
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt   # import the pyplot from matplotlib
 gene_lengths = [9410, 394141, 4442, 105338, 19149, 76779, 126550, 36296, 842, 15981]
 exon_counts = [51, 1142, 42, 216, 25, 650, 32533, 57, 1, 523]
 # input the data
 average_length = []  # just set up a new list which is used for ave_axon length
-for i in range(1, 10):
+for i in range(0, 10):
     average_length.append(gene_lengths[i]/exon_counts[i])
     # calculate the average length with for command so we don't ne to do the calculation one by one
 average_length.sort()    # just sort the average list
 print(average_length)  # out put the result and data
-plt.boxplot(average_length, vert=True, whis=1.5, patch_artist=False, meanline=True, showbox=True, showcaps=True, showfliers=True, notch=True)
-# make a boxplot
+plt.boxplot(average_length, labels=["The Average length"])  # make a boxplot
+plt.title("The Average length")  # title the box plot
 plt.show()
 # show the box plot
-
--- Practical 6 code 0004 piechart.py
+++ Practical 6 code 0004 piechart.py
@@ -1,8 +1,9 @@
-import matplotlib.pyplot as plt
-labels = "USA", "India", "Brazil", "Russia", "UK"
-size = [29862124, 11285561, 11205972, 4360823, 4234924]
-explode = (0.1, 0.1, 0.1, 0.05, 0)
+import matplotlib.pyplot as plt          # import matplotlib to make a pie chart
+labels = "USA", "India", "Brazil", "Russia", "UK"           # label the pie of the pie chart
+size = [29862124, 11285561, 11205972, 4360823, 4234924]           # input the data of each pie of the pie chart
+explode = (0.1, 0, 0, 0, 0)       # define the position of each pie of the pie chart
 plt.pie(size, explode=explode, labels=labels, autopct="%1.1f%%", shadow=False, startangle=90)
-plt.axis("equal")
-plt.show()
-exit()
+plt.axis("equal")    # make a the pie chart is a circle
+plt.title("The Condition of Covid-19 Of 5 Countries")
+plt.show()        # show the pie chart
+exit()         # end the program
