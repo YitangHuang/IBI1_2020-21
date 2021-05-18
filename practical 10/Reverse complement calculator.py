a54c7802e1978285b912117d9578a851d3f23126
YitangHuang <yitang.20@intl.zju.edu.cn>
2021-05-18 23:11:12 +0800

    Add a example

--- practical 10/Reverse complement calculator.py
+++ practical 10/Reverse complement calculator.py
@@ -27,3 +27,14 @@ print("".join(calculator(DNA_Seq)))
 
 # Converts the list to string output
 # out put the outcome
+# -----------------------------------------------------------------------------------------
+# exammple:
+# if the sequence user entered is: AaaTttcgGGGCaTTTTTtttttGGGGGGGG
+# The outcome would be :TTTAAAGCCCCGTAAAAAAAAAACCCCCCCC
+
+# results
+# C:\Users\Razer\AppData\Local\Programs\Python\Python38\python.exe "C:/Users/Razer/PycharmProjects/practical 10/Reverse complement calculator.py"
+# This is a reverse complement calculator. Please input the DNA sequence:AaaTttcgGGGCaTTTTTtttttGGGGGGGG
+# TTTAAAGCCCCGTAAAAAAAAAACCCCCCCC
+#
+# Process finished with exit code 0
